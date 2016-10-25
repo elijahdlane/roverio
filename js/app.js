9851926d9c452bd5d9858bb3945521bf233876b9
Bunta <BuntaFujiwara@Buntas-MacBook-Pro.local>
2016-10-25 11:54:53 -0400

    Show photo div on click

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var app = angular.module('roverio', []);
 
 app.controller('roverController', function ($scope, $http) {
+    $scope.showThis = false;
     $scope.submit = function () {
 
 
@@ -8,6 +9,7 @@ app.controller('roverController', function ($scope, $http) {
   $http.get("https://api.nasa.gov/mars-photos/api/v1/rovers/" + $scope.rover + "/photos?earth_date=" + $scope.year + "-" + $scope.month + "-" + $scope.day + "&camera=" + $scope.camera + "&api_key=bQA1iWCDiQu6uw8E7OkoosqcZdEbkLjA6H8CeT2j")
         .then(function (response) {
             $scope.roverdata = response.data;
+            $scope.showThis = true;
 
         });
 
