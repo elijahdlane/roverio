cf4678316a00acf7c577abbe078dd27cb1e8819a
Michael Kates <michaelkates@ITs-MacBook-Pro.local>
2016-10-25 12:37:17 -0400

    Update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 var app = angular.module('roverio', []);
 
 app.controller('roverController', function ($scope, $http) {
+
+    $scope.showThis = false;
     $scope.submit = function () {
 
 
@@ -8,7 +10,8 @@ app.controller('roverController', function ($scope, $http) {
   $http.get("https://api.nasa.gov/mars-photos/api/v1/rovers/" + $scope.rover + "/photos?earth_date=" + $scope.year + "-" + $scope.month + "-" + $scope.day + "&camera=" + $scope.camera + "&api_key=bQA1iWCDiQu6uw8E7OkoosqcZdEbkLjA6H8CeT2j")
         .then(function (response) {
             $scope.roverdata = response.data;
-
+            
+             $scope.showThis = true;
         });
 
 
--- server.js
+++ server.js
@@ -1,22 +1,27 @@
-var express = require ('express');
-var app = express ();
-var path = require ('path');
-var http = require ('http');
+var express = require('express');
+var app = express();
+var path = require('path');
+var http = require('http');
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log ("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
-app.use(express.static(__dirname + '/node_modules'));
-app.use(express.static(__dirname + '/scripts'));
-app.use(express.static(__dirname + '/styles'));
-
-app.use (function (req, res, next) {
- res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
-app.get('/', function(req, res){ 
+app.use("/node_modules", express.static(__dirname + '/node_modules'));
+app.use("/js", express.static(__dirname + '/js'));
+app.use("/css", express.static(__dirname + '/css'));
+app.use("/img", express.static(__dirname + '/img'));
+app.use("/fonts", express.static(__dirname + '/fonts'));
+
+
+
+app.get('/', function (req, res) {
+
     res.sendFile(path.join(__dirname + '/index.html'));
 });
