060057e6d6fe78fd7c37d9a28632ca25cbdfa3ff
Elijah Lane <elijah.lane@eku.edu>
2016-10-27 13:08:10 -0400

    Updates

--- js/app.js
+++ js/app.js
@@ -9,12 +9,11 @@ app.controller('roverController', function ($scope, $http) {
 
         $http.get("https://api.nasa.gov/mars-photos/api/v1/rovers/" + $scope.rover + "/photos?earth_date=" + $scope.year + "-" + $scope.month + "-" + $scope.day + "&camera=" + $scope.camera + "&api_key=bQA1iWCDiQu6uw8E7OkoosqcZdEbkLjA6H8CeT2j")
             .then(function (response) {
-                $scope.roverdata = response.data;
+                $scope.roverphoto = response.data;
                 $scope.showThis = true;
 
 
             });
-
 
     }
 });
